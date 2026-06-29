251b8a7b421d255111b128100fc0af55d4fe2fa8
cyber-gentle <info.abdavid@gmail.com>
2026-06-29 13:56:06 +0100

    feat: add func main and homehandler to render pages

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseGlob("templates/html/*.html"))
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", nil)
+}
+
+func Server() {
+	mainMux := http.NewServeMux()
+	apiMux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("templates/assets/"))
+	http.Handle("/templates/assets/imgs", http.StripPrefix("/templates/assets/imgs/", fileServer))
+
+	mainMux.HandleFunc("/", HomeHandler)
+	http.Handle("/api/", http.StripPrefix("/api", apiMux))
+	log.Println("Server started on http://localhost:8080")
+	http.ListenAndServe(":8080", mainMux)
+}
+
+func renderTemplate(w http.ResponseWriter, filename string, data any) {
+	err := tmpl.ExecuteTemplate(w, filename, data)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error rendering template: %v", err)
+	}
+}
+
+func main() {
+	Server()
+}
