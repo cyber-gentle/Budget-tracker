940678c3b2c6cb26b5d4af65ede1554115219c87
cyber-gentle <info.abdavid@gmail.com>
2026-06-30 09:02:27 +0100

    feat: add connector funcc

--- main.go
+++ main.go
@@ -12,15 +12,30 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html", nil)
 }
 
+func SignUpHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "sign-up.html", nil)
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "login.html", nil)
+}
+
+func SwitchHandler() {
+	
+}
+
 func Server() {
-	mainMux := http.NewServeMux()
-	apiMux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("templates/assets/"))
 	http.Handle("/templates/assets/imgs", http.StripPrefix("/templates/assets/imgs/", fileServer))
 
+	mainMux := http.NewServeMux()
+	apiMux := http.NewServeMux()
+
 	mainMux.HandleFunc("/", HomeHandler)
-	http.Handle("/api/", http.StripPrefix("/api", apiMux))
+	mainMux.HandleFunc("/sign-up.html", SignUpHandler)
+	mainMux.Handle("/api/", http.StripPrefix("/api", apiMux))
+
 	log.Println("Server started on http://localhost:8080")
 	http.ListenAndServe(":8080", mainMux)
 }
